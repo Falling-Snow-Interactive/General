75aefec52249e68623e4367c4ed90a9211de3606
Kira Dinan <kiradinan.dev@gmail.com>
2026-01-23 14:49:03 -0400

    Update FsiSceneManager.cs

--- Runtime/SceneManagement/FsiSceneManager.cs
+++ Runtime/SceneManagement/FsiSceneManager.cs
@@ -9,33 +9,51 @@ namespace Fsi.General.SceneManagement
 	{
 		private static bool DebugLog => FsiSceneManagerUtility.Settings.DebugLog;
 
+		// ReSharper disable Unity.PerformanceAnalysis
 		public void LoadScene(string sceneName, LoadSceneMode mode)
 		{
 			SceneManager.LoadScene(sceneName, mode);
-			if (DebugLog) Debug.Log($"SCENE - Loaded {sceneName}");
+			if (DebugLog)
+			{
+				Debug.Log($"SceneManagement | Loaded {sceneName}");
+			}
 		}
 
+		// ReSharper disable Unity.PerformanceAnalysis
 		public void LoadSceneAsync(string sceneName, LoadSceneMode mode, Action onComplete = null)
 		{
-			if (DebugLog) Debug.Log($"SCENE: Loading {sceneName}");
+			if (DebugLog)
+			{
+				Debug.Log($"SceneManagement | Loading {sceneName}");
+			}
 
 			AsyncOperation sceneAsync = SceneManager.LoadSceneAsync(sceneName, mode);
 			if (sceneAsync != null)
 			{
-				if (DebugLog) Debug.Log($"SCENE: Loaded {sceneName}");
+				if (DebugLog)
+				{
+					Debug.Log($"SceneManagement | Loaded {sceneName}");
+				}
 
 				sceneAsync.completed += _ => onComplete?.Invoke();
 			}
 		}
 
+		// ReSharper disable Unity.PerformanceAnalysis
 		public void UnloadScene(string sceneName, Action onComplete = null)
 		{
-			if (DebugLog) Debug.Log($"SCENE: Unloading {sceneName}");
+			if (DebugLog)
+			{
+				Debug.Log($"SceneManagement | Unloading {sceneName}");
+			}
 
 			AsyncOperation sceneAsync = SceneManager.UnloadSceneAsync(sceneName);
 			if (sceneAsync != null)
 			{
-				if (DebugLog) Debug.Log($"SCENE: Unloaded {sceneName}");
+				if (DebugLog)
+				{
+					Debug.Log($"SceneManagement | Unloaded {sceneName}");
+				}
 
 				sceneAsync.completed += _ => onComplete?.Invoke();
 			}
