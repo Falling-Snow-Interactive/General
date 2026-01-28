2df3767c7d6792c820867b0f7657daaf4531df20
Kira Dinan <kiradinan.dev@gmail.com>
2026-01-28 16:34:57 -0400

    Icons!

---
+++ Editor/Icons/IconPaths.cs
@@ -0,0 +1,13 @@
+namespace Fsi.General.Icons
+{
+    public static class IconPaths
+    {
+        public const string Open = "Packages/com.fallingsnowinteractive.general/Assets/Icons/Open_Icon.png";
+
+        public const string Character = "Packages/com.fallingsnowinteractive.general/Assets/Icons/Character_Icon.png";
+        public const string Character_Grey = "Packages/com.fallingsnowinteractive.general/Assets/Icons/Character_Icon_Grey.png";
+        
+        public const string Debug = "Packages/com.fallingsnowinteractive.general/Assets/Icons/Debug_Icon.png";
+        public const string Debug_Grey = "Packages/com.fallingsnowinteractive.general/Assets/Icons/Debug_Icon_Grey.png";
+    }
+}
